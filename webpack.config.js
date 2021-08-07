f15150570c2c851ca660b79562a7f64932d7ea47
Noupin <naperkins7@gmail.com>
2021-08-07 19:25:03 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.ts',
+  entry: './src/index.ts',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$|tsx/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      { test: /\.css$/, use: 'css-loader' }
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'webpack.bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
