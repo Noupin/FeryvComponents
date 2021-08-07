5ce78b74a9e679a33229f7249fa5ab023150b3db
Noupin <naperkins7@gmail.com>
2021-08-07 18:49:18 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,21 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].bundle.js'
-    },
-    devtool: "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
-            }
-        ]
-    },
-
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html"
-        })
-    ]
+  entry: './index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
