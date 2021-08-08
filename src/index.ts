31b01f7a38fa86538fac1a997f46c620ba16470a
Noupin <naperkins7@gmail.com>
2021-08-08 13:52:58 -0400

    updated exporting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import './index.css';
 import './index.scss';
 
-
 export * from './Components/FileDialog/FileDialog';
 export * from './Components/HorizontalScrollMenu/HorizontalScrollMenu';
 export * from './Components/Image/Image';
 export * from './Components/Masonry/HorizontalMasonry';
 export * from './Components/Media/Media';
 export * from './Components/Video/Video';
+export * from './Components/ProtectedRoute/ProtectedRoute';
+
+export * as neumorphic from './neumorphic';
+export * as glassmorphic from './glassmorphic';
--- src/react-router.ts
+++
@@ -1,5 +0,0 @@
-import './index.css';
-import './index.scss';
-
-
-export * from './Components/ProtectedRoute/ProtectedRoute';
