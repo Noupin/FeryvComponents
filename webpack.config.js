a0ddf380668bcc5efb42bf8c5a32b30be9201c9c
Noupin <naperkins7@gmail.com>
2021-08-09 11:15:21 -0400

    updated webpack with react externals

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,23 @@ module.exports = {
       chunkFilename: "[id].css",
     }),
   ],
+  externals: [
+    // nodeExternals(),
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
+    }
+  ],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
