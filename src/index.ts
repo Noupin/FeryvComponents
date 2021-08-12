4d7200743a1f6573db65c2119c0862811e283424
Noupin <naperkins7@gmail.com>
2021-08-12 16:40:39 -0400

    exported media list

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export * from './Components/Image/Image';
 export * from './Components/Masonry/HorizontalMasonry';
 export * from './Components/Media/Media';
 export * from './Components/Video/Video';
+export * from './Components/MediaList/MediaList';
 export * from './Components/ProtectedRoute/ProtectedRoute';
 
 export * from './neumorphic';
